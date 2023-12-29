da4fc14075b13c44ecbd025b52a93e2fe7f4ca2b
Hanif Al Irsyad <hanifalirsyad.123@students.amikom.ac.id>
2023-12-29 21:33:40 +0700

    update with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Hanifanta/go-hello
+module github.com/Hanifanta/go-hello/v2
 
 go 1.21.5
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,5 @@
 package go_hello
 
-func SayHello() string {
-	return "Hello World! from go-hello"
+func SayHello(name string) string {
+	return "Hello " + name
 }
